d9aeac8fcab1e25cb2cccac0e50a821bfcfd3edf
CSchechtman <craigschechtman@gmail.com>
2024-03-19 15:39:17 -0400

    end of class commit

---
+++ Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/practice/Variables-Practice-2.js
@@ -0,0 +1,36 @@
+// Create a variable for each aspect of an address 
+// First Name, Last Name, House-Number, Street-Name, City, State, Zip
+// Combine them to be usable as a postage address
+const fName = 'Craig'
+const lName = 'Schechtman'
+const houseNum = '5'
+const street = 'Grace Park Drive'
+const city = 'Commack'
+const state = 'New York'
+const zip = '11725'
+const address = `${fName} ${lName} 
+${houseNum}, ${street}
+${city}, ${state}
+${zip}`
+
+console.log(address)
+
+/*
+First-Name Last-Name
+House-Number, Street-Name 
+City, State
+Zip
+
+Your log should look like this:
+
+Steve Rogers
+569, Amsterdam Ave
+New York, NY
+18062 
+*/
+
+const address = `${fName} ${lName} 
+${houseNum}, ${street}
+${city}, ${state}
+${zip}`
+
--- Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/practice/variable-notes.js
+++ Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/practice/variable-notes.js
@@ -7,3 +7,24 @@ console.log(birthYear)
 
 const fName = 'Craig'
 console.log(fName)
+
+const color1 = 'red'
+{
+  console.log(color1) // red
+}
+
+{
+  const color2 = 'yellow'
+}
+console.log(color2) // not defined
+
+// now using var
+var color3 = 'blue'
+{
+  console.log(color3) // blue
+}
+
+{
+  var color4 = 'purple'
+}
+console.log(color4) // purple
