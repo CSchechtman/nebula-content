0231cbff2830607dc165efb267aefd8937cd48d1
CSchechtman <craigschechtman@gmail.com>
2024-03-19 20:39:11 -0400

    Variable Homework

--- Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/assignments/Variable-Homework.js
+++ Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/assignments/Variable-Homework.js
@@ -7,4 +7,33 @@
 
 //1. Create a set of variables that define an animal (age, height, name, etc...). Must be at least 6 variables.
 
+const animalName = 'Black Panther'
+const color = "Black"
+const age = 20
+const height = 25
+const topSpeed = 36
+const habitat = "Jungle"
+
+console.log(animalName)
+console.log(color)
+console.log(age)
+console.log(height)
+console.log(topSpeed)
+console.log(habitat)
+
 //2. Create a set of variables that define a vehicle. Must be at least 6 variables.
+
+const make = 'Acura'
+const model = 'Integra'
+const carColor = 'Grey'
+const acceleration = 7
+const outputHP = 200
+const weight = 3073
+
+
+console.log(make)
+console.log(model)
+console.log(carColor)
+console.log(acceleration)
+console.log(outputHP)
+console.log(weight)
