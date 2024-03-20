4baa8950b9c0cb3d197e22b050a954a701c1c64f
CSchechtman <craigschechtman@gmail.com>
2024-03-20 10:52:19 -0400

    Advanced Practice

---
+++ Module-1-Introduction-to-Software-Engineering/Lesson-3-JavaScript-Basics/practice/Numbers-Practice-0.js
@@ -0,0 +1,34 @@
+// Log the results to the console
+
+
+// 1. Add together two numbers.
+x = 2 + 3
+console.log(x)
+
+// 2. Subtract two numbers.
+y = 3 - 1
+console.log(y)
+
+// 3. Multiply two numbers.
+z = 4 * 3
+console.log(z)
+
+// 4. Divide two numbers.
+a = 3 / 2
+console.log(a)
+
+// 5. Find the remainder of 223 divided by 3.
+b = 223 % 3
+console.log(b)
+
+// 6. Store the value in a variable produced by dividing 300 by 100. Declare that value with the `let` keyword.
+let c = 300 / 100
+console.log(c)
+
+// 7. Increment that variable by 1 using the increment operator.
+c += 1
+console.log(c)
+
+// 8. Use a multiply assignment operator to change the value of the variable to the product of multiplying it by 5.
+c *= 5
+console.log(c)
